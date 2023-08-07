244e60bae34316207299268d49a9cf2e142b2e22
Michal Skvely <m.skvely@make.com>
2023-08-07 15:36:18 +0200

    Remove unintentional logging

--- src/isolate/generic/object.h
+++ src/isolate/generic/object.h
@@ -1,18 +1,5 @@
 #pragma once
 #include <v8.h>
-#include <iostream>
-
-void PrintV8Object(v8::Local<v8::Context> context, v8::Local<v8::Object> object) {
-    v8::Local<v8::Array> property_names = object->GetOwnPropertyNames(context).ToLocalChecked();
-
-    for (unsigned int i = 0; i < property_names->Length(); i++) {
-        v8::Local<v8::Value> key = property_names->Get(context, i).ToLocalChecked();
-        v8::String::Utf8Value utf8_key(context->GetIsolate(), key);
-        v8::Local<v8::Value> value = object->Get(context, key).ToLocalChecked();
-        v8::String::Utf8Value utf8_value(context->GetIsolate(), value);
-        std::cout << *utf8_key << ": " << *utf8_value << '\n';
-    }
-}
 
 namespace ivm {
     inline auto GetObjectOwnProperties(v8::Local<v8::Context> context, v8::Local<v8::Object> object) -> v8::Local<v8::Array> {
@@ -22,8 +9,6 @@ namespace ivm {
     }
 
     inline void CopyObjectProperties(v8::Local<v8::Context> context, v8::Local<v8::Object> target, v8::Local<v8::Object> source, v8::Local<v8::Array> property_names) {
-        PrintV8Object(context, source);
-
         for (uint32_t i = 0; i < property_names->Length(); i++) {
             v8::MaybeLocal<v8::Value> key = property_names->Get(context, i);
             if (key.IsEmpty()) {
